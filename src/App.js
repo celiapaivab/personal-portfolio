f7cb3a84f822090e864f0bae96336e898d992ef3
celiapaivab <celiadepaivabruno@gmail.com>
2024-10-21 20:12:02 -0500

    Banner section complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <NavBar />
+    <div className='App'>
+      <NavBar />
+      <Banner />
+    </div>
+    
   );
 }
 
